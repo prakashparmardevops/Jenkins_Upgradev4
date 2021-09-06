34912c95902c5ce2291d8538af24c8d6281d8165
Prakash Parmar <prakashtparmar@gmail.com>
2021-09-06 13:43:58 +0530

    c

--- java-tomcat-sample/MavenProjectDSL.groovy
+++ java-tomcat-sample/MavenProjectDSL.groovy
@@ -1,5 +1,5 @@
 job( "Firest-Maven-Project-Via-DSL" ) {
-    discriptio ("First Maven Project Create Using DSL ${new Date()}, on this date ")
+    discription ("First Maven Project Create Using DSL ${new Date()}, on this date ")
 scm{
     git("https://github.com/prakashparmardevops/Jenkins_Upgradev4.git", main)  
 }
